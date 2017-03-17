f15f12cc0f3747f5e4e0f959a5cb7c838dffdc94
shaoyunchen <nefu_shaoyun@163.com>
2017-03-17 16:55:26 +0800

    remove target info

--- target/classes/static/js/parentAuto.js
+++
@@ -1,5 +0,0 @@
-window.onload=function(){
-	console.log(document.body.clientHeight);
-	parent.document.all("autoIframe").style.height=document.body.clientHeight+"px";
-	//parent.document.all("autoIframe").style.width=document.body.scrollWidth;
-};
